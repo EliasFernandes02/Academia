3c7e4f90113e0c9acaa525bcf93162f08b1f6238
Elias Leite <103540031+EliasFernandes02@users.noreply.github.com>
2022-06-06 12:48:27 -0300

    Initial commit

---
+++ Js/javascript.js
@@ -0,0 +1,42 @@
+window.onscroll = () =>{
+    menubar.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+   loop:true,
+   
+   autoplay:{
+     delay:7500,
+     disableOnInteraction:false,
+
+   },
+   breakpoints:{
+     0:{
+       slidesPerView:1,
+     },
+     600:{
+       slidesPerView:2,
+     }
+   }
+  });
+
+  
